2a5b39144376a030f1a15be514048e1c0a072688
ryrie <ryrie106@gmail.com>
2019-05-10 12:45:47 +0900

    Initial React.js Setting

---
+++ src/main/java/io/github/ryrie/vidflow/VidflowApplication.java
@@ -0,0 +1,13 @@
+package io.github.ryrie.vidflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidflowApplication.class, args);
+    }
+
+}
