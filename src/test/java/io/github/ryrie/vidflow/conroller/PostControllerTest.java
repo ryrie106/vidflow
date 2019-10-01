518d0a278a12b8ce8f52a1b9af571c7c2628225e
ryrie <ryrie106@gmail.com>
2019-10-01 19:45:06 +0900

    PostControllerTest 추가

---
+++ src/test/java/io/github/ryrie/vidflow/conroller/PostControllerTest.java
@@ -0,0 +1,42 @@
+package io.github.ryrie.vidflow.conroller;
+
+import io.github.ryrie.vidflow.service.PostService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private PostService postService;
+
+    @Before
+    public void setup() throws Exception {
+    }
+
+    @Test
+    public void getPostIdTest() throws Exception {
+        mockMvc.perform(get("/posts/postId"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(postService.getPostId().toString()));
+    }
+}
